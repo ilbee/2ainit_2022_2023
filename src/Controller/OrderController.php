a4d548ebc049df05303662fd9b1019f18abec458
Julien PRIGENT <julien.prigent@fnacdarty.com>
2023-02-09 11:03:58 +0100

    Mise à jour

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Order;
 use App\Form\OrderType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -19,10 +21,19 @@ class OrderController extends AbstractController
     }
 
     #[Route('/order/new', name: 'app_order_new')]
-    public function new(): Response
+    public function new(
+        EntityManagerInterface $entityManager,
+        Request $request
+    ): Response
     {
         $order = new Order();
         $form = $this->createForm(OrderType::class, $order);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($order);
+            $entityManager->flush();
+        }
 
         return $this->render('order/new.html.twig', [
             'controller_name' => 'OrderController',
